db6da2f24319d67482c121fcbfefe2457a2ab450
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-08-22 16:33:01 +0400

    waste

--- src/moduls/user-accounts/decarators/current-user.ts
+++ src/moduls/user-accounts/decarators/current-user.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: string | undefined, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    console.log('CurrentUser decorator2:', request.user);
 
     if (!request.user) return null;
 
