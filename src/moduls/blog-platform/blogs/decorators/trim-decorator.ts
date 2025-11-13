05b7d78e18f2669798b5dde84f57bfef04824d68
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-11-13 12:32:31 +0400

    trim decor

--- src/moduls/blog-platform/blogs/decorators/trim-decorator.ts
+++ src/moduls/blog-platform/blogs/decorators/trim-decorator.ts
@@ -1,7 +1,7 @@
 import { Transform, TransformFnParams } from 'class-transformer';
 
 // Custom decorator (в библиотеке class-transformer по умолчанию нету декоратора trim)
-// не забываем установить transform: true в глобальном ValidationPipe
+// не забываем установить transform: true в глобальном ValidationPipe а то атата
 export const Trim = () => {
   return Transform(({ value }: TransformFnParams) => {
     return typeof value === 'string' ? value.trim() : value;
